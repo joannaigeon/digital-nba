238fe44714c13301cead8e63d1c34628b56536b5
joannaigeon <joan.naigeon1@gmail.com>
2021-10-04 15:44:52 +0200

    Update Home.js

--- assets/components/Home.js
+++ assets/components/Home.js
@@ -4,10 +4,22 @@ import React, {useState} from 'react';
 const Home = () => {
     const [message, setMessage] = useState('Coucou');
     return (
-        <div>
-            <h1>{message} les gens</h1>
-            <button onClick={() => setMessage('bonjour')}>bouh</button>
-        </div>
+
+        <main>
+        	<div class="container">
+        		<div class="row">
+	        		<div class="col-2"></div>
+	        		<div class="col-8">
+        		        <div>
+            				<h1>{message} les gens</h1>
+            				<button onClick={() => setMessage('bonjour')}>bouh</button>
+        					</div>
+        				</div>
+	        		<div class="col-2"></div>
+	        	</div>
+        	</div>
+        </main>
+
     )
 }
 export default Home;
