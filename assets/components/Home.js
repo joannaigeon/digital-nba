a576afc88dae939ad0504e1ddd682739cb97cc33
joannaigeon <joan.naigeon1@gmail.com>
2021-10-04 16:54:53 +0200

    uh

--- assets/components/Home.js
+++ assets/components/Home.js
@@ -1,24 +1,35 @@
 import React, {useState} from 'react';
+import Header from './Header';
 
 
 const Home = () => {
     const [message, setMessage] = useState('Coucou');
     return (
 
-        <main>
+        <div>
+        <Header/>
         	<div class="container">
         		<div class="row">
-	        		<div class="col-2"></div>
-	        		<div class="col-8">
-        		        <div>
-            				<h1>{message} les gens</h1>
-            				<button onClick={() => setMessage('bonjour')}>bouh</button>
-    					</div>
-    				</div>
-	        		<div class="col-2"></div>
+                    <div class="col"></div>
+                    <div class="col-8 tableau">
+                        <div class="row match">
+        	        		<div class="col-4">
+                                <div class="team-logo"><img src="https://upload.wikimedia.org/wikipedia/fr/thumb/d/de/Warriors_de_Golden_State_logo.svg/361px-Warriors_de_Golden_State_logo.svg.png"/></div>
+                                <div class="team-name"><h2>lorem ipsum</h2></div>
+                            </div>
+        	        		<div class="col-4 score">
+                                <h4>X - X</h4>
+            				</div>
+        	        		<div class="col-4">
+                                <div class="team-logo"><img src="https://upload.wikimedia.org/wikipedia/fr/thumb/d/de/Warriors_de_Golden_State_logo.svg/361px-Warriors_de_Golden_State_logo.svg.png" /></div>
+                                <div class="team-name"><h2>lorem ipsum</h2></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col"></div>
 	        	</div>
         	</div>
-        </main>
+        </div>
 
     )
 }
